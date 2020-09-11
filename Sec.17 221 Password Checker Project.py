06d3200436e1a4f865722eed4d40821efab5c40f
Olexandr Chesnakov <olexandrchess@gmail.com>
2020-09-10 22:17:31 -0700

    Add files from video 226

--- Sec.17 221 Password Checker Project.py
+++ Sec.17 221 Password Checker Project.py
@@ -8,12 +8,24 @@
 # https://passwordsgenerator.net/sha1-hash-generator/
 
 import requests
+import hashlib
 
 # password123 - SHA1 - CBFDAC6008F9CAB4083784CBD1874F76618D2A97
 # API accept 5 first symbols from SHA1
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
-res = requests.get(url)
+def request_api_data(query_char):
+    """Provides API with the first 5 symbols of SHA1 hash of the passoword and get response if it was pwned"""
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
 
-print (res)
+def pwned_api_check(password):
+    """Check Password if it exists in API response"""
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1password
 
+
+print(request_api_data('CBFDA'))
+print(pwned_api_check('password123'))
