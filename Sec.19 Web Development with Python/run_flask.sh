3a012a2d44469d78fa78936c52f4d7aa7dc88f5f
Olexandr Chesnakov <olexandrchess@gmail.com>
2020-09-18 12:23:48 -0700

    Add files from videos 258-261

--- Sec.19 Web Development with Python/run_flask.sh
+++ Sec.19 Web Development with Python/run_flask.sh
@@ -3,5 +3,6 @@
 source ./venv/bin/activate && echo Virtual Environment is activated
 
 export FLASK_APP=server.py
+export FLASK_ENV=development
 
-flask run&
+flask run
--- Sec.19 Web Development with Python/server.py
+++ Sec.19 Web Development with Python/server.py
@@ -5,6 +5,7 @@
 
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/
 
+
 # We need to run:
 # $ source ./venv/bin/activate
 # $ export FLASK_APP=server.py
@@ -12,10 +13,19 @@
 # $ flask run
  
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Alex2!'
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('index.html', name =username, post_id=post_id)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return 'This is my blog.'
+
