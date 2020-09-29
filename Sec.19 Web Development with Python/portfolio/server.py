9eef87e4151181adbf76d4983dfbcb7478b2b7a5
Olexandr Chesnakov <olexandrchess@gmail.com>
2020-09-29 13:00:42 -0700

    Changes for videos 273-275

--- Sec.19 Web Development with Python/portfolio/server.py
+++ Sec.19 Web Development with Python/portfolio/server.py
@@ -40,9 +40,12 @@ def html_page(page_name):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data=request.form.to_dict()
-        write_to_csv(data)
-        return redirect('/thankyou.html')
+        try:
+            data=request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
     else:
         return 'something went wrong'
 
