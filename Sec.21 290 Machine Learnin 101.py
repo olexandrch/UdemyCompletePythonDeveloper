48e92726710a68f4ec5e47a8d502f1f17ffa3ef9
Olexandr Chesnakov <olexandrchess@gmail.com>
2021-01-26 20:48:18 -0800

    Add lessons 285-290

---
+++ Sec.21 290 Machine Learnin 101.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# https://www.udemy.com/course/complete-python-developer-zero-to-mastery/
+
+# 290. Machine Learning 101
+
+# https://repl.it/@aneagoie/MLSteps
+# https://research.fb.com/blog/2018/05/the-facebook-field-guide-to-machine-learning-video-series/
+
+#1 - Import the data
+#2 - Clean the data
+#3 - Split data. Training Set/Test Set
+#4 - Create a Model 
+#5 - Check the output
+#6 - Improve
