51132ff0e04f6237f54c2a40254f9df70c6f31e1
Olexandr Chesnakov <olexandrchess@gmail.com>
2021-01-27 20:22:48 -0800

    Add lessons 291-301

---
+++ Sec.21 292 Data Science 1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# https://www.udemy.com/course/complete-python-developer-zero-to-mastery/
+
+# 291. Tools In Machine Learning
+
+# https://numpy.org/
+# https://pandas.pydata.org/                    Data Analysis, work with .csv
+# https://scikit-learn.org/stable/index.html    ML Algorithms
+# https://matplotlib.org/                       Plots and Visualization
+# https://www.anaconda.com/products/individual  For Jupiter Notebooks
+# https://www.kaggle.com/                       Datasets and Jupiter Notebooks examples
+
+# 1. Install Anaconda
+# 2. start Jupiter notebook
+# 3. Download a dataset from https://www.kaggle.com/   
+# 4. manipulate data https://elitedatascience.com/python-cheat-sheet
+
---
+++ Sec.21 295 Machine Learning 1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# https://www.udemy.com/course/complete-python-developer-zero-to-mastery/
+
+# 291. Tools In Machine Learning
+
+# https://numpy.org/
+# https://pandas.pydata.org/                    Data Analysis, work with .csv
+# https://scikit-learn.org/stable/index.html    ML Algorithms
+# https://matplotlib.org/                       Plots and Visualization
+# https://www.anaconda.com/products/individual  For Jupiter Notebooks
+# https://www.kaggle.com/                       Datasets and Jupiter Notebooks examples
+
